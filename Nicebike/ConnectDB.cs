e8db2ad669528906c014c33bc328cf42a0374931
Sébastien Bokken <sebastien@cche-pc23-ltp.vinci.be>
2023-03-30 16:18:34 +0200

    ajout connexion DB

---
+++ Nicebike/ConnectDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using MySql.Data.MySqlClient;
+
+
+namespace Nicebike
+{
+    public static class Db
+    {
+        public readonly static string connectionString = "server=pat.infolab.ecam.be;port=63309;database=dbNicebike;user=projet_gl;password=root;";
+
+        public static void Opebn()
+        {
+            MySqlConnection connection = new(connectionString);
+            connection.Open();
+        }
+
+    }
+
+}
